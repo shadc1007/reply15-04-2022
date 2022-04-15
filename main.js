8ce60bb1ebaf565098337a6e1d5ae625de52a96b
shadrack <103117823+shadc1007@users.noreply.github.com>
2022-04-15 17:39:01 +0800

    firstcommit

---
+++ main.js
@@ -0,0 +1,10 @@
+let name = document.querySelector("#name-el")
+console.log(name)
+let goodEl = document.querySelector("#good-el")
+
+function greeting(nameEl) {
+  name.innerHTML = "hii " + nameEl + "!!, di ako makakapagvalo kase ayoko dejk kase di nako marunong HAHAHAHAHAHA last yr pa last na laro ko dudzzszszszsdsz"
+}
+
+var userInput = prompt("sino ka muna?")
+greeting(userInput)
